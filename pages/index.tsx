518a70061073884c1a8c273be30d8051ddbfaff9
tom <tom@axisj.com>
2022-09-06 01:44:50 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import type {NextPage} from 'next';
+import Head from 'next/head';
+import {Container} from '../styles/Layouts';
+import dynamic from 'next/dynamic';
+import styled from '@emotion/styled';
+
+const Home: NextPage = () => {
+  return (
+    <PageContainer>
+      <Head>
+        <title>react-frame-table</title>
+        <meta name='description' content='Index'/>
+        <link rel='icon' href='/favicon.ico'/>
+      </Head>
+
+      <Container>
+        <h1>Sample</h1>
+
+      </Container>
+    </PageContainer>
+  );
+};
+
+const PageContainer = styled.div`
+  padding: 0 20px;
+`;
+
+export default Home;
