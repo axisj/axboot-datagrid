5da6424affaf3765e7a53bdf2bbfd6cf957aa595
tom <tom@axisj.com>
2022-09-27 12:11:21 +0900

    0.1.0
    remove unused code

--- react-frame-datagrid/store/createAppStore.ts
+++ react-frame-datagrid/store/createAppStore.ts
@@ -2,9 +2,5 @@ import createContext from 'zustand/context';
 import { AppStore } from '../types';
 import { StoreApi } from 'zustand';
 
-const {
-  Provider: AppStoreProvider,
-  useStore: useAppStore,
-  useStoreApi: useAppStoreApi,
-} = createContext<StoreApi<AppStore<any>>>();
-export { AppStoreProvider, useAppStore, useAppStoreApi };
+const { Provider: AppStoreProvider, useStore: useAppStore } = createContext<StoreApi<AppStore<any>>>();
+export { AppStoreProvider, useAppStore };
